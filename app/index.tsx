114f876039e62c6341f5b6bb10e31ebb5c496276
William Armstrong <william.armstrong967@gmail.com>
2025-02-23 15:05:50 -0600

    Add core stopwatch and lap functionality. Saving current progress before I go ham trying to fix the clearInterval issue.

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Text, View, Pressable } from "react-native";
 import styles from "./styles";
 
 export default function Index() {
-  //functions and variables here
-  const [isPaused, setIsPaused] = useState(false);
+  const [paused, setpaused] = useState(false);
+  const [time, settime] = useState([0, 0, 0]);
+  const [laptimes, setlaptimes] = useState([]);
+  const formattedtime = `${String(time[2]).padStart(2, "0")}:${String(
+    time[1]
+  ).padStart(2, "0")}:${String(time[0]).padStart(2, "0")}`;
+
+  let IntervalID = setInterval(() => 1000);
+  clearInterval(IntervalID); //doesn't work
 
   const togglewatch = () => {
-    setIsPaused(!isPaused);
+    setpaused(!paused);
+    if (!paused) {
+      IntervalID = setInterval(() => {
+        //here
+        settime((prev) => {
+          let [hundredths, seconds, minutes] = prev;
+          hundredths++;
+          if (hundredths === 60) {
+            hundredths = 0;
+            seconds++;
+          }
+          if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+          }
+          return [hundredths, seconds, minutes];
+        });
+      }, 10);
+    } else {
+      clearInterval(IntervalID);
+    }
   };
 
-  const stop = () => {};
+  const stop = () => {
+    setpaused(false);
+    clearInterval(IntervalID);
+    settime([0, 0, 0]);
+  };
 
-  const lap = () => {};
+  const lap = () => {
+    if (paused) {
+      setlaptimes([time, ...laptimes]);
+    }
+  };
 
-  const deleteLap = () => {};
+  const deleteLap = (index: number) => {
+    const newlaptimes = [...laptimes];
+    newlaptimes.splice(index, 1);
+    setlaptimes(newlaptimes);
+  };
 
-  //screen here
   return (
     <View style={styles.appcontainer}>
-      <Text style={styles.time}>00:00:00</Text>
+      <Text style={styles.time}>{formattedtime}</Text>
       <View style={styles.buttonbox}>
         <Pressable
           onPress={togglewatch}
-          style={isPaused ? styles.pause : styles.start}
+          style={paused ? styles.pause : styles.start}
         >
-          <Text style={styles.buttontext}>{isPaused ? "I I" : "▶"}</Text>
+          <Text style={styles.buttontext}>{paused ? "I I" : "▶"}</Text>
         </Pressable>
         <Pressable onPress={stop} style={styles.stop}>
           <Text style={styles.stoptext}>▪</Text>
@@ -35,12 +73,21 @@ export default function Index() {
         <Text style={styles.buttontext}>Lap</Text>
       </Pressable>
       <View style={styles.lapbox}>
-        <View style={styles.laptime}>
-          <Text style={styles.laptext}>00:00:00</Text>
-          <Pressable onPress={deleteLap} style={styles.lapdelete}>
-            <Text style={styles.deletetext}>x</Text>
-          </Pressable>
-        </View>
+        {laptimes.map((laptime, index) => (
+          <View key={index} style={styles.laptime}>
+            <Text style={styles.laptext}>
+              {String(laptime[2]).padStart(2, "0")}:
+              {String(laptime[1]).padStart(2, "0")}:
+              {String(laptime[0]).padStart(2, "0")}
+            </Text>
+            <Pressable
+              onPress={() => deleteLap(index)}
+              style={styles.lapdelete}
+            >
+              <Text style={styles.deletetext}>x</Text>
+            </Pressable>
+          </View>
+        ))}
       </View>
     </View>
   );
